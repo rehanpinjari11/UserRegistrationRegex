eddc7be334ba1e20558c83c202c573ba6f0d2f8c
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-06 15:11:52 +0530

    Added: Welcome Message is added

---
+++ src/com/bl/java/regex/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.bl.java.regex;
+
+public class UserRegistration {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to User Registration Problem");
+    }
+}
