6933e46b55f3209a525d27d9f87b2ff5616cadf1
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-09 11:03:57 +0530

    Added: Checking First Name is Valid Or Not

--- src/com/bl/java/regex/UserRegistration.java
+++ src/com/bl/java/regex/UserRegistration.java
@@ -1,9 +1,31 @@
 package com.bl.java.regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
+    static Scanner sc = new Scanner(System.in);
+
+    // Method to check first name is valid or not
+    public static void firstName()
+    {
+        System.out.println("Enter Your First Name: ");
+        String firstName = sc.nextLine();
+
+        if (Pattern.matches("^[A-Z][a-zA-Z]{2,}$", firstName))
+        {
+            System.out.println("First Name is Valid");
+        }
+        else
+        {
+            System.out.println("Invalid first name! Please enter a name starting with capital letter and with a minimum 3 characters.");
+        }
+
+    }
     public static void main(String[] args)
     {
-        System.out.println("Welcome to User Registration Problem");
+        // Calling the method
+        firstName();
     }
 }
