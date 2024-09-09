d1b781a714a87ae30141f6dfa3824deb7e3c1591
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-09 11:14:36 +0530

    Added: Checking Last Name is Valid Or Not

--- src/com/bl/java/regex/UserRegistration.java
+++ src/com/bl/java/regex/UserRegistration.java
@@ -13,6 +13,7 @@ public class UserRegistration {
         System.out.println("Enter Your First Name: ");
         String firstName = sc.nextLine();
 
+        // Regex pattern: Start with a capital letter and have a minimum of 3 characters
         if (Pattern.matches("^[A-Z][a-zA-Z]{2,}$", firstName))
         {
             System.out.println("First Name is Valid");
@@ -23,9 +24,28 @@ public class UserRegistration {
         }
 
     }
+
+    // Method to check last name is valid or not
+    public static void lastName()
+    {
+        System.out.println("Enter Your Last Name: ");
+        String lastName = sc.nextLine();
+
+        // Regex pattern: Start with a capital letter and have a minimum of 3 characters
+        if (Pattern.matches("^[A-Z][a-zA-Z]{2,}$", lastName))
+        {
+            System.out.println("Last Name is Valid");
+        }
+        else
+        {
+            System.out.println("Invalid last name! Please enter a name starting with capital letter and with a minimum 3 characters.");
+        }
+
+    }
     public static void main(String[] args)
     {
         // Calling the method
         firstName();
+        lastName();
     }
 }
